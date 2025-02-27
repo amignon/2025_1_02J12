bd52f323e90d665d7d70bf72d82488e37e9ca12e
amignon <amignon@gmail.com>
2025-02-27 01:24:22 +0000

    Enunciado

--- aula_03/Curtidas.java
+++ aula_03/Curtidas.java
@@ -1,5 +1,15 @@
 import java.util.Scanner;
-
+/**
+ * Imagine que você foi contratado para desenvolver um sistema de análise de engajamento 
+ * para uma rede social, onde os usuários interagem curtindo as publicações. 
+ * Neste exercício, seu programa deverá solicitar ao usuário o número de publicações que ele 
+ * deseja analisar. Em seguida, para cada publicação, o programa pedirá que o usuário 
+ * informe quantas curtidas ela recebeu. Para cada publicação, verificar se o número de curtidas 
+ * é igual ou superior a 100. Caso seja, o programa deve exibir a mensagem “Publicação popular!”. 
+ * Se não, exibir a mensagem “Publicação com poucas curtidas.”. Ao final, o sistema deve 
+ * apresentar ao usuário a média de curtidas das publicações analisadas, 
+ * além de informar o total de publicações processadas.
+ */
 public class Curtidas {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
